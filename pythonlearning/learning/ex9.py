70128c97c7535de17d265041641eda751ac35a08
anirudhmehta-tradedoubler <anime@TD9145.root.tradedoubler.com>
2015-09-23 17:06:04 +0200

    Adding ex9

---
+++ pythonlearning/learning/ex9.py
@@ -0,0 +1,11 @@
+__author__ = 'anime'
+days="Mon Tue Wed Thur Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days:", days)
+print("Here are the months:",months)
+
+print("")
+print("""There is something going on here. With three double-quotes.
+We will be able to type as much as we like.
+Even 4 lines or 5 lines or 6,7,8.""")
